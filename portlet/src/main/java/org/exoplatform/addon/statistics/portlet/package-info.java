ce2f8f4c1a4ef2faee996f253b63a7b3dc8bec2c
ngammoudi <klmnitts1980>
2016-10-04 13:59:33 +0100

    Add datatables export to csv

--- portlet/src/main/java/org/exoplatform/addon/statistics/portlet/package-info.java
+++ portlet/src/main/java/org/exoplatform/addon/statistics/portlet/package-info.java
@@ -1,14 +1,21 @@
 @Portlet
 @Application(name = "HibernateStatistics")
 @Bindings({@Binding(HibernateStatisticsService.class)})
-@Scripts({
-        @Script(id = "jquery-dataTables-js", value = "js/datatable/jquery.dataTables.min.js"),
-        @Script(id = "datatable-js", value = "js/datatable/dataTables.bootstrap.min.js",depends = {"jquery-dataTables-js"}),
-        @Script(id = "hibernate-statistics-js", value = "js/hibernatestatistics/hibernatestatistics.js",depends = {"datatable-js"})
-           })
-@Stylesheets({ @Stylesheet(id = "datatable-css", value = "css/datatable/dataTables.bootstrap.min.css"),
-        @Stylesheet(id = "bootstrap-css", value = "css/datatable/bootstrap.min.css"),
-               @Stylesheet(id = "hibernate-statistics-css", value = "css/hibernatestatistics/hibernatestatistics.css")})
+@Scripts({@Script(id = "jquery", value = "js/jquery/jquery-1.12.3.js"),
+        @Script(id = "datatable-js", value = "js/datatable/jquery.dataTables.min.js"),
+        @Script(id = "datatable-buttons-js", value = "js/datatable/dataTables.buttons.min.js"),
+        @Script(id = "flash-buttons-js", value = "js/datatable/buttons.flash.min.js"),
+        @Script(id = "jszip-js", value = "js/datatable/jszip.min.js"),
+        @Script(id = "pdfmake-js", value = "js/datatable/pdfmake.min.js"),
+        @Script(id = "vfs-fonts-js", value = "js/datatable/vfs_fonts.js"),
+        @Script(id = "html5-buttons-js", value = "js/buttons/buttons.html5.min.js"),
+        @Script(id = "print-buttons-js", value = "js/buttons/buttons.print.min.js"),
+        @Script(id = "hibernate-statistics-js", value = "js/hibernatestatistics/hibernatestatistics.js",depends = {"jquery","datatable-js"})
+})
+@Stylesheets({ @Stylesheet(id = "datatable-css", value = "css/datatable/jquery.dataTables.min.css"),
+        @Stylesheet(id = "buttons-css", value = "css/datatable/buttons.dataTables.min.css"),
+        @Stylesheet(id = "jquery-ui-css", value = "css/jquery/jquery-ui.css"),
+        @Stylesheet(id = "hibernate-statistics-css", value = "css/hibernatestatistics/hibernatestatistics.css")})
 @Assets("*")
 package org.exoplatform.addon.statistics.portlet;
 
